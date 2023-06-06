93064dcb9264d017531149ee00c11d0faa628a32
MiyuLove <122012237+MiyuLove@users.noreply.github.com>
2023-06-06 19:56:44 +0900

    LogIn UI Activity

---
+++ LoginActivity.kt
@@ -0,0 +1,149 @@
+package com.chatproject.chatcafeproject
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chatproject.chatcafeproject.Logic.LogInActivityLogic
+import com.chatproject.chatcafeproject.ui.theme.ChatCafeProjectTheme
+
+lateinit var lc : LogInActivityLogic
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lc = LogInActivityLogic(this)
+        setContent {
+            ChatCafeProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginScreen(){
+    Column() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+        {
+            LoginText("블 란 서", 90)
+
+        }
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .weight(3f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ){
+            Column {
+                Row{
+                    LoginText("ID", 60,
+                    modifier = Modifier.weight(1f),
+                    textAlign = TextAlign.End)
+
+                    PasswordTextField(txt = "안녕하세요", modifier =
+                    Modifier.weight(4f))
+                }
+                LoginText("ID를 기억 못해요?", 20)
+            }
+            Column{
+                Row{
+                    LoginText("PW", 60)
+                    PasswordTextField(txt = "안녕하세요", modifier =
+                    Modifier.weight(4f))
+                }
+                LoginText("비밀번호도 모르겠네?", 20)
+            }
+
+        }
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+            horizontalAlignment = Alignment.CenterHorizontally
+            ){
+            Button(
+                onClick = { lc.logInBtnClick() },
+
+            ){
+                Text("들 어 가 기", fontSize = 70.sp,
+                    fontFamily = FontFamily(Font(R.font.base_font))
+                )
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun LoginText(text : String, fontSize : Int
+){
+    Text(text = text, fontSize = fontSize.sp)
+}
+
+@Composable
+fun LoginText(text : String, fontSize : Int, modifier: Modifier,
+              textAlign: TextAlign
+){
+    Text(text = text, fontSize = fontSize.sp, modifier = modifier, textAlign = textAlign)
+}
+
+@Composable
+fun PasswordTextField(
+    txt : String, modifier: Modifier
+) {
+    var text by remember{ mutableStateOf(TextFieldValue(txt)) }
+
+    TextField(
+        value = text,
+        onValueChange = { newText -> text = newText },
+        modifier = modifier,
+        placeholder = {Text("*")}
+    )
+//    var password by rememberSaveable { mutableStateOf("") }
+
+  /*  TextField(
+        value = password,
+        onValueChange = { password = it },
+        label = { Text("Enter password") },
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+    )*/
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview5() {
+    ChatCafeProjectTheme {
+        LoginScreen()
+    }
+}
