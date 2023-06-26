0d326104a3877eeed5ee1f8a5c7d96cad9be6726
MiyuLove <122012237+MiyuLove@users.noreply.github.com>
2023-06-27 02:54:57 +0900

    FinalLogin
    if it is edited next time, just padding or margin etc... relative to ratio issue.

--- LoginActivity.kt
+++ LoginActivity.kt
@@ -11,22 +11,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.exercise.cafechatmaterial3ver.Logic.LogInActivityLogic
 import com.exercise.cafechatmaterial3ver.Logic.LoginView
 import com.exercise.cafechatmaterial3ver.composeAct.LogTextField
+import com.exercise.cafechatmaterial3ver.composeAct.LoginButton
+import com.exercise.cafechatmaterial3ver.ui.theme.ColorCopy
 
 lateinit var lc : LogInActivityLogic
 lateinit var la : Activity
@@ -50,59 +47,50 @@ fun LoginScreen(){
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom,
     ){
-        Spacer(modifier = Modifier.weight(.5f))
+        Spacer(modifier = Modifier.weight(.2f))
+        Text(
+            "ChatCafe", textAlign = TextAlign.Center, color = ColorCopy().TitleColor,
+            fontSize = 80.sp,
+        )
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Bottom,
-            modifier = Modifier
-                .weight(1f),
-
+            modifier = Modifier.weight(1f),
         )
         {
-            LogTextField("ID",viewModel = loginView, 0, modifier = Modifier.padding(bottom = 20.dp))
-            LogTextField("PW",viewModel = loginView, 1, modifier = Modifier.padding(bottom = 20.dp))
-
+            LogTextField("ID",viewModel = loginView, 0,
+                modifier = Modifier.fillMaxWidth().padding(start = 40.dp, bottom = 20.dp, end = 40.dp), captionPadding = 40)
+            LogTextField("PW",viewModel = loginView, 1,
+                modifier = Modifier.fillMaxWidth().padding(start = 40.dp, bottom = 20.dp, end = 40.dp), captionPadding = 40)//will be changed the PW LogTextField
         }
 
         Row(modifier = Modifier
             .weight(1f)
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(start = 40.dp, end = 40.dp),
             verticalAlignment = Alignment.Top,
             horizontalArrangement = Arrangement.SpaceEvenly
             ){
-            Button(
-                modifier = Modifier.weight(.4f).padding(start = 20.dp, end = 10.dp),
+            LoginButton(
+                modifier = Modifier
+                    .weight(.4f),
                 onClick = {
-            //        lc.logInBtnClick()
-                    },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.main_color)
-                )
-
-            ){
-                Text("로 그 인", fontSize = 30.sp,
-                    fontFamily = FontFamily(Font(R.font.base_font)),
-                    maxLines = 1,
-                    overflow = TextOverflow.Clip
-                )
-            }
-
-            Button(
-                modifier = Modifier.weight(.4f).padding(start = 10.dp, end = 20.dp),
-                onClick = {
-                    lc.logInBtnClick(Intent(la,SignForActivity::class.java))
+                    la.startActivity(Intent(la,ViewActivity::class.java))
+                    la.finish()
                 },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = colorResource(id = R.color.main_color)
-                )
-
-            ){
-                Text("회 원 가 입", fontSize = 30.sp,
-                    fontFamily = FontFamily(Font(R.font.base_font)),
-                    maxLines = 1,
-                    overflow = TextOverflow.Clip
-                )
-            }
+                text = "시 작 하 기",
+                fontSize = 30
+            )
+            Spacer(Modifier.weight(.1f))
+            LoginButton(
+                modifier = Modifier
+                    .weight(.4f),
+                onClick = {
+                    la.startActivity(Intent(la,SignForActivity::class.java))
+                          },
+                text = "회 원 가 입",
+                fontSize = 30
+            )
         }
     }
 }
